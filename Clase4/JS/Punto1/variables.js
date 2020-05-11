b032534bb5c945bcf58ad7db9da5cb00f0a8543e
JoaquinPettinari <Joaquinpettinari@hotmail.com>
2020-05-11 13:34:20 -0300

    Clase 4

---
+++ Clase4/JS/Punto1/variables.js
@@ -0,0 +1,12 @@
+var a = 10
+var b = 5
+var c = 5
+
+var d = a + b + c 
+
+console.log(d)
+
+a = b
+b = 15
+
+console.log(a)
---
+++ Clase4/JS/Punto2/mensajeVariables.js
@@ -0,0 +1,7 @@
+var nombre = "Joaquin"
+var edad = "21"
+var cumple = "24/11/1998"
+var ciudad = "Buenos Aires"
+var ocupacion = "Alumno asistente"
+
+console.log(`Buenos dias, mi nombre es ${nombre} tengo ${edad} y cumplo el ${cumple}. Actualmente estoy viviendo en ${ciudad} y soy ${ocupacion} en la Universidad Nacional de Hurlingham`)
